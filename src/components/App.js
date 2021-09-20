d07e589b9d669ac21ed0121fe21596f6bb807c9a
Danielle Thompson <danithompson74@gmail.com>
2021-09-19 19:25:50 -0700

    Create V basic app component w/ h1 and calendar

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import '../App.css';
+import Calendar from "./calendar.js";
 
 function App() {
   return (
-    <h1>Testing 1, 2, 3</h1>
+    <>
+      <h1 style={{textAlign: 'center'}}>Code School Q and A</h1>
+      <Calendar></Calendar>
+    </>
   );
 }
 
